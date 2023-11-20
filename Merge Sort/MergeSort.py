d26ce642cd1d8114f615bab0613c9f1ff6037977
igor-bertelli <igor.bertelli@mail.usf.edu.br>
2023-11-20 13:33:01 -0300

    refactor MergeSort

--- Merge Sort/MergeSort.py
+++ Merge Sort/MergeSort.py
@@ -1,65 +1,55 @@
 import time
 import random
 
-def merge_sort(arr):
-    trocas = 0
-    comparacoes = 0
+def merge(left, right, compare):
+    result = []
+    i = j = comp = trocas = 0
+    while i < len(left) and j < len(right):
+        comp += 1
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+            trocas += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result, comp, trocas
 
-    def merge(left, right):
-        nonlocal trocas, comparacoes
-        result = []
-        i = j = 0
+def merge_sort(arr, compare = lambda x, y: x < y):
+    if len(arr) < 2:
+        return arr[:], 0, 0
+    else:
+        middle = len(arr) // 2
+        left, comp_left, trocas_left = merge_sort(arr[:middle], compare)
+        right, comp_right, trocas_right = merge_sort(arr[middle:], compare)
+        merged, comp_merge, trocas_merge = merge(left, right, compare)
+        return merged, comp_left + comp_right + comp_merge, trocas_left + trocas_right + trocas_merge
 
-        while i < len(left) and j < len(right):
-            comparacoes += 1
-            if left[i] <= right[j]:
-                result.append(left[i])
-                i += 1
-            else:
-                result.append(right[j])
-                j += 1
-                trocas += len(left) - i  # Conta as trocas ao inserir elementos da metade direita
-
-        result.extend(left[i:])
-        result.extend(right[j:])
-        return result
-
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-    sorted_arr = merge(left, right)
-
-    return sorted_arr, trocas, comparacoes
-
-def run_merge_sort_case(case, size):
-    if case == "melhor":
-        arr = list(range(1, size + 1))  # Melhor Caso: Valores em ordem crescente
-    elif case == "medio":
-        arr = random.sample(range(1, size + 1), size)  # Caso Médio: Valores desordenados com números aleatórios
-    elif case == "pior":
-        arr = list(range(size, 0, -1))  # Pior Caso: Valores em ordem decrescente
-
-    start_time = time.time()
-    sorted_arr, trocas, comparacoes = merge_sort(arr.copy())
-    end_time = time.time()
-
-    execution_time = end_time - start_time
-    return execution_time, trocas, comparacoes
-
-# Teste para vetores de tamanho 1000, 10000, 100000
 sizes = [1000, 10000, 100000]
-
 for size in sizes:
-    print(f"Tamanho do vetor: {size}")
+    # Melhor caso
+    arr = list(range(size))
+    start_time = time.time()
+    sorted_arr, comp, trocas = merge_sort(arr)
+    end_time = time.time()
+    print(f"Melhor caso com {size} elementos - Tempo: {end_time - start_time}, Comparações: {comp}, Trocas: {trocas}")
 
-    melhor_caso = run_merge_sort_case("melhor", size)
-    medio_caso = run_merge_sort_case("medio", size)
-    pior_caso = run_merge_sort_case("pior", size)
+    # Caso médio
+    arr = random.sample(range(size), size)
+    start_time = time.time()
+    sorted_arr, comp, trocas = merge_sort(arr)
+    end_time = time.time()
+    print(f"Caso médio com {size} elementos - Tempo: {end_time - start_time}, Comparações: {comp}, Trocas: {trocas}")
 
-    print(f"Melhor Caso: Tempo de Execução = {melhor_caso[0]:.6f}s, Quantidade de Trocas = {melhor_caso[1]}, Quantidade de Comparações = {melhor_caso[2]}")
-    print(f"Caso Médio: Tempo de Execução = {medio_caso[0]:.6f}s, Quantidade de Trocas = {medio_caso[1]}, Quantidade de Comparações = {medio_caso[2]}")
-    print(f"Pior Caso: Tempo de Execução = {pior_caso[0]:.6f}s, Quantidade de Trocas = {pior_caso[1]}, Quantidade de Comparações = {pior_caso[2]}")
-    print("-" * 50)
+    # Pior caso
+    arr = list(range(size, 0, -1))
+    start_time = time.time()
+    sorted_arr, comp, trocas = merge_sort(arr, compare = lambda x, y: x > y)
+    end_time = time.time()
+    print(f"Pior caso com {size} elementos - Tempo: {end_time - start_time}, Comparações: {comp}, Trocas: {trocas}")
